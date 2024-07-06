d662116264739c736455e5b2ae2b3e5cc05ebc28
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-07-06 16:01:57 +0200

    add icons navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
 
 import Connection from "./pages/Connection";
 import Home from "./pages/Home";
