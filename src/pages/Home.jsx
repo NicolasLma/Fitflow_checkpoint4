7e408ba1b7f54991fdfe39bd308a893d7d624a9d
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-07-06 17:54:15 +0200

    page home finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
+import imgHome from "../assets/accueil.jpg";
+import iconesFitflowWhite from "../assets/logo_fitflow_white.png";
+
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-      <p>toto</p>
-    </>
+    <section className="home_container">
+      <section className="home_content">
+        <img className="bg_img" src={imgHome} alt="" />
+        <img className="icon_fitflow" src={iconesFitflowWhite} alt="" />
+        <h2 className="mess_bvn">
+          Bienvenus chez <span className="bold">FITFLOW</span> <br />{" "}
+          <span className="no_pain">
+            Planifiez votre entraînement avec nous
+          </span>
+        </h2>
+      </section>
+      <Link className="button_home">
+        <button>Connexion</button>
+      </Link>
+      <Link className="button_home">
+        <button>Inscription</button>
+      </Link>
+    </section>
   );
 }
