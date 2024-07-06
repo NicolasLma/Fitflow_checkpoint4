90d9a688dccebae0bf5ba318d3188ace0330bc19
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-07-06 21:02:49 +0200

    add datas muscles

--- src/pages/Exercices.jsx
+++ src/pages/Exercices.jsx
@@ -1,8 +1,21 @@
+import "../styles/Exercices.css"
+import NavBar from "../components/NavBar"
+
+import logoFitflowBlack from "../assets/logo_fitflow_black.png"
+
 export default function Exercices() {
   return (
     <>
-      <h1>Exercices</h1>
-      <p>titi</p>
+    <section className="exercices_container">
+      <section className="logo_center">
+      <img src={logoFitflowBlack} alt="" />
+      </section>
+        <section className="exercices_content">
+<h1>EXERCICES</h1>
+<h2>Exercices par groupe musculaire</h2>
+        </section>
+    </section>
+    <NavBar/>
     </>
   );
 }
