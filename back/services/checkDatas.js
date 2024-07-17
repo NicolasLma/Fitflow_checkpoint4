8cb3cd78631b04531d2aec28ca30384e9e6dfb51
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-07-17 13:36:19 +0200

    add checklogindatas in checkdatas

--- back/services/checkDatas.js
+++ back/services/checkDatas.js
@@ -5,13 +5,31 @@ const checkRegisterDatas = (req, res, next) => {
     email: Joi.string().email().presence("required"),
     password: Joi.string().min(6).max(20).presence("required"),
     firstname: Joi.string().min(2),
-    lastname: Joi.string().min(2)
-  }).validate(req.body, {abortEarly : false});
+    lastname: Joi.string().min(2),
+  }).validate(req.body, { abortEarly: false });
 
-if (!error) {
+  if (!error) {
     next();
-} else {
+  } else {
     res.status(400).json(error);
-}
+  }
+};
 
+const checkLoginDatas = (req, res, next) => {
+  const { error } = Joi.object({
+    email: Joi.string().email().presence("required"),
+    password: Joi.string().min(6).max(20).presence("required"),
+  }).validate(req.body, { abortEarly: false });
+
+  if (!error) {
+    next();
+  } else {
+    console.error("Valikdation error :", error);
+    res.status(400).json(error);
+  }
+};
+
+module.exports = {
+  checkRegisterDatas,
+  checkLoginDatas,
 };
