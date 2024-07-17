79e44f465427f70a82ae45f6b44131eecb62dc0a
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-07-17 11:21:15 +0200

    auth and usenavigate component navbar

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,22 +1,42 @@
 import "../styles/NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+
+import { useAuth } from "../context/AuthContext";
+
 
 import iconProfile from "../assets/icons-user.png";
 import iconExercices from "../assets/icons-fitness.png";
 import iconPrograms from "../assets/icons-planning.png";
 
 export default function NavBar() {
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth) {
+      navigate("/");
+    }
+  }, [auth, navigate]);
+
   return (
     <section className="nav_container">
-      <Link to="/exercices">
-        <img src={iconExercices} alt="" />
-      </Link>
-      <Link to="/programmes">
-        <img src={iconPrograms} alt="" />
-      </Link>
-      <Link to="/profil">
-        <img src={iconProfile} alt="" />
-      </Link>
+     {auth ? (
+        <>
+          <Link to="/exercices">
+            <img src={iconExercices} alt="Exercices Icon" />
+          </Link>
+          <Link to="/programmes">
+            <img src={iconPrograms} alt="Programs Icon" />
+          </Link>
+          <Link to="/profil">
+            <img src={iconProfile} alt="Profile Icon" />
+          </Link>
+        </>
+      ) : (
+        null
+      )}
     </section>
   );
 }
